e8539b6f280d385997189f04e88669ce6fd792ce
Yosoi <yosoi@protonmail.com>
2020-06-22 22:00:34 -0400

    cut bloat and install dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-grid.css';
+import 'bootstrap/dist/css/bootstrap-reboot.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
